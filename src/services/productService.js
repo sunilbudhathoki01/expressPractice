1d46da9ee8f87252cf26b81d9f08fff44f4259ea
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-08-06 07:25:22 +0545

    business logic in productServive

--- src/services/productService.js
+++ src/services/productService.js
@@ -3,7 +3,9 @@ import fs from "fs"
 const getproducts=(req,res)=>{
     const rawData=fs.readFileSync("./src/data/products.json","utf8");
     const data=JSON.parse(rawData);
-    const filteredData=data.filter(p=>p.price>60)
+    const filteredData=data.filter(p=>p.price==1219.99)
+if(!filteredData)return res.status(401).send("not found")
+    
         return filteredData;
 
 }
